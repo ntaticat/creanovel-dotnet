4def74dbe051d8ec7805114b8de95e0bc0a5f020
Rafael Estrada <ntaticat@gmail.com>
2024-02-10 00:30:51 -0600

    fix dockerfile

--- commands.sh
+++ commands.sh
@@ -3,3 +3,7 @@ dotnet ef migrations add NOMBRE_MIGRACION -p Persistence/ -s WebAPI/
 
 # Actualizar la base de datos
 dotnet ef database update --project WebAPI/
+
+# Crear imagen y ejecutar contenedor
+docker build -f WebAPI/Dockerfile -t creanovel-api .   
+docker run -d -p 5000:80 --name creanovel-api creanovel-api
