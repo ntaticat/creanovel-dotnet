28e77fb3d956683cfe942b7f7f352184deec5621
Rafael Estrada <ntaticat@gmail.com>
2025-12-04 17:16:48 -0600

    add auto database update in Program.cs

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,11 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace WebAPI
 {
@@ -13,7 +10,19 @@ namespace WebAPI
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var env = scope.ServiceProvider.GetRequiredService<IHostEnvironment>();
+                {
+                    var db = scope.ServiceProvider.GetRequiredService<CreanovelDbContext>();
+                    db.Database.Migrate();
+                }
+
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
